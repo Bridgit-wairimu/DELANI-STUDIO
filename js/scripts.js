07d0822dfbab8c92ebdc98a6c066faf279048323
Bridgit-wairimu <kirikabetty@gmail.com>
2020-10-11 07:57:26 -0700

    deleted jQuery.js file

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".show").toggle();
+    $(".hide").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".show").toggle();
+    $(".hide").toggle();
+  });
+});
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".show").toggle();
+    $(".hide").toggle();
+  });
+});
